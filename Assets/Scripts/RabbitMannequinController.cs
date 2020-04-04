2ad0c9cb0dd17c4d1f9f8f18b704e02162a9c402
0testarossa <lumina.fatetestarossa@gmail.com>
2020-04-04 23:25:56 +0200

    add music to gameplay and fix cinematic script

--- Assets/Scripts/RabbitMannequinController.cs
+++ Assets/Scripts/RabbitMannequinController.cs
@@ -72,8 +72,6 @@ public class RabbitMannequinController : MonoBehaviour
 
     void Movement()
     {
-        //if (controller.isGrounded)
-        //{
 
         if(state == "begin")
         {
@@ -102,47 +100,6 @@ public class RabbitMannequinController : MonoBehaviour
         {
             transform.Rotate(0, 1, 0);
         }
-
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    anim.SetInteger("condition", 1);
-        //    moveDir = Vector3.forward;
-        //    moveDir *= speed;
-        //    moveDir = transform.TransformDirection(moveDir);
-        //}
-        else if (Input.GetKey(KeyCode.S))
-        {
-            anim.SetInteger("condition", 1);
-            moveDir = new Vector3(0, 0, -1);
-            moveDir *= speed;
-            moveDir = transform.TransformDirection(moveDir);
-        }
-        //else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-        //{
-        //    anim.SetInteger("condition", 0);
-        //    moveDir = new Vector3(0, 0, 0);
-        //}
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            //rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-            transform.Rotate(0, -1, 0);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            //rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-            transform.Rotate(0, 1, 0);
-        }
-        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            transform.Rotate(0, 0, 0);
-        }
-
-        //rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-        //transform.eulerAngles = new Vector3(0, rot, 0);
-
-        //}
-        //transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
 
         moveDir.y -= gravity * Time.deltaTime;
         controller.Move(moveDir * Time.deltaTime);
